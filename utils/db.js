8f09b1f46c4bb0a950b05104c6749082b2cdf8ce
Kaarel <kaarel.viilvere@voco.ee>
2025-01-31 09:54:15 +0200

    Add project files + start script

--- utils/db.js
+++ utils/db.js
@@ -3,20 +3,20 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345',
+    password: 'qwerty', //12345
     database: 'joga_mysql'
 })
 
 connection.connect((err) => {
     if(err) throw err
     console.log('MySQL server is connected')
-    let sql = 'INSERT INTO customers (name, address) VALUES'
-    ('John', 'Highway 71');
-    connection.query(sql, (err, result) =>{
-        console.log(result)
-        if(err) throw err
-        console.log('Number of records inserted: ' + result.affectedRows)
-    })
+    // let sql = 'INSERT INTO customers (name, address) VALUES'
+    // ('John', 'Highway 71');
+    // connection.query(sql, (err, result) =>{
+    //     console.log(result)
+    //     if(err) throw err
+    //     console.log('Number of records inserted: ' + result.affectedRows)
+    // })
 })
 
 module.exports = connection
